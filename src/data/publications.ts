c4b391f792988d5fc2191c21ffb6a7d41358cde0
Peylics <lisichen_2901@outlook.com>
2025-10-22 20:38:19 -0400

    Update the content

--- src/data/publications.ts
+++ src/data/publications.ts
@@ -1,20 +1,20 @@
 import type { Publication } from '../types';
 
 export const publications: Publication[] = [
-  {
-    title: "EmoSense: A Hybrid CNN-RAG Framework for FER-Based Facial Emotion Analysis and Mental Health Support",
-    authors: "Sichen Li, et al.",
-    venue: "IEEE International Conference on Image Processing (ICIP)",
-    year: "2025",
-    type: "conference",
-    links: {
-      paper: "#",
-      status: "Submitted"
-    }
-  },
+  // {
+  //   title: "EmoSense: A Hybrid CNN-RAG Framework for FER-Based Facial Emotion Analysis and Mental Health Support",
+  //   authors: "Sichen Li, et al.",
+  //   venue: "IEEE International Conference on Image Processing (ICIP)",
+  //   year: "2025",
+  //   type: "conference",
+  //   links: {
+  //     paper: "#",
+  //     status: "Submitted"
+  //   }
+  // },
   {
     title: "An intelligent imposition method for card printing",
-    authors: "Sichen Li, et al.",
+    authors: "Yanting Lu, Weilian Lu, Qing Mi, Libiao Chen, Zian Tian, Wei Ma, Sichen Li, Zhengzheng Tian, Jinliang Dong, Dongmei Jiang, and Bing Li",
     venue: "China National Intellectual Property Administration",
     year: "2024",
     type: "patent",
